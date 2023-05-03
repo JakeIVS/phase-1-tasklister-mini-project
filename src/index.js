d9a2c9ea444d27c4d58781bd07f22ee63f2bbe60
Jacob Ives <jacob.w.ives@gmail.com>
2023-05-03 13:11:57 -0600

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('form');
+  const toDoList = document.querySelector('ul');
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let btn = document.createElement('button')
+    btn.textContent = 'remove';
+    btn.addEventListener('click', ()=>{
+      li.remove();
+    })
+    let li = document.createElement('li');
+    let newTask = e.target.querySelector('#new-task-description').value;
+    li.textContent = newTask+"   ";
+    toDoList.append(li);
+    li.append(btn);
+    form.reset();
+  })
 });
