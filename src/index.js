50453751021e65a95ef4fb9e0440c6edee03876a
Jacob Ives <jacob.w.ives@gmail.com>
2023-05-03 17:11:36 -0600

    add color coding priority list

--- src/index.js
+++ src/index.js
@@ -12,6 +12,19 @@ document.addEventListener("DOMContentLoaded", () => {
     let li = document.createElement('li');
     let newTask = e.target.querySelector('#new-task-description').value;
     li.textContent = newTask+"   ";
+    let priority = form.querySelector('#priority-list')
+    let priorityVal = priority.value;
+    console.log(priorityVal)
+    if (priorityVal === 'high') {
+      colorVar = 'red';
+    } else if (priorityVal === 'medium') {
+      colorVar = 'orange';
+    } else if (priorityVal === 'low') {
+      colorVar = 'green';
+    } else {
+      colorVar = 'black';
+    }
+    li.style.color = colorVar;
     toDoList.append(li);
     li.append(btn);
     form.reset();
